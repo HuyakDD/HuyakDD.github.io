0fa2f311742f811f11fd72a8d28b163abb90a65f
Vladimir Okun <24977978+okvv@users.noreply.github.com>
2019-12-24 03:33:30 +0200

    init
    testing React GA

---
+++ precache-manifest.c764693463670410909d8a5f84c95661.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5f70b0e7acba217de59d67e1e12fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcda6ef76364ac3f6a2d",
+    "url": "/static/css/2.a4ac9b21.chunk.css"
+  },
+  {
+    "revision": "04b28f3772ca0714eff2",
+    "url": "/static/css/main.53858d8d.chunk.css"
+  },
+  {
+    "revision": "dcda6ef76364ac3f6a2d",
+    "url": "/static/js/2.ad8c37ed.chunk.js"
+  },
+  {
+    "revision": "04b28f3772ca0714eff2",
+    "url": "/static/js/main.6ec71ae1.chunk.js"
+  },
+  {
+    "revision": "c61c027e80fab518a7ed",
+    "url": "/static/js/runtime~main.09664b48.js"
+  },
+  {
+    "revision": "5c72e214bd12c1c367aa0a13087128e7",
+    "url": "/static/media/big-slot-machine.5c72e214.jpg"
+  },
+  {
+    "revision": "3baf88a863c7a6f8e8aa38f9aedb953c",
+    "url": "/static/media/casino.3baf88a8.jpeg"
+  },
+  {
+    "revision": "3d044acfe27f2bae018457c304625dce",
+    "url": "/static/media/slot-machine.3d044acf.jpg"
+  },
+  {
+    "revision": "9bf0152bc2ad3c9bbdd1ccd781219a54",
+    "url": "/static/media/slot-sprites.9bf0152b.png"
+  }
+]);
